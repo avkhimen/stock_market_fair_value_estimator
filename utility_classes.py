dbd80ee85549e24312778bc6a73b5331ae08115e
avkhimen <avkhimen@ualberta.ca>
2020-03-02 13:15:59 -0700

    Created the 'Symbols' class that will return all available company symbols

--- utility_classes.py
+++ utility_classes.py
@@ -1,11 +1,18 @@
 
 
-class FairValue()
-	def __init__(symbol, regression_data_points, num_financial_eps):
+class FairValue():
+	def __init__(self, symbol, regression_data_points, num_financial_eps):
 		pass
 
 	def grab_real_time_stock_price(self):
 		pass
 
 	def grab_financial_statements(self):
+		pass
+
+class Symbols:
+	def __init__(self):
+		pass
+
+	def return_all_symbols(self):
 		pass
