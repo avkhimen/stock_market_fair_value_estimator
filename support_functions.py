7fee89e8648d265fafcc88b465cbdbe0e3ec8cb2
avkhimen <avkhimen@ualberta.ca>
2020-03-02 16:23:09 -0700

    Introduced 'get_input_args' function to get market for query

---
+++ support_functions.py
@@ -0,0 +1,8 @@
+import argparese
+
+def get_input_args():
+    """Returns input arguments for main file execution"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--market', '-m', type = str, default = 'NYSE', 
+                        help = 'Market to query stock symbols')
+    return parser.parse_args()
