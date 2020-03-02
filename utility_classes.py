eaa4859c471620d56395f7e5e9254606d39cad04
avkhimen <avkhimen@ualberta.ca>
2020-03-02 13:13:53 -0700

    Created the 'FairValue' class that will manipulate symbol financials

---
+++ utility_classes.py
@@ -0,0 +1,11 @@
+
+
+class FairValue()
+	def __init__(symbol, regression_data_points, num_financial_eps):
+		pass
+
+	def grab_real_time_stock_price(self):
+		pass
+
+	def grab_financial_statements(self):
+		pass
