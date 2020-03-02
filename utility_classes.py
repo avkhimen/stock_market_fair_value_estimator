7766698d9459bdb8f5fbc0a91af26c0cd27dc6d4
avkhimen <avkhimen@ualberta.ca>
2020-03-02 16:14:48 -0700

    Completed a working function to get all symbols names for one market

--- utility_classes.py
+++ utility_classes.py
@@ -3,15 +3,17 @@ import json
 from pprint import pprint
 
 class FairValue():
-	def __init__(self, symbol, regression_data_points, num_financial_eps, Y = 1.8, no_growth_eps = 7, growth_factor = 1):
+	def __init__(self, symbol, regression_data_points, num_financial_eps, market, Y = 1.8, no_growth_eps = 7, growth_factor = 1):
 		self.symbol = symbol
 		self.regression_data_points = regression_data_points
 		self.num_financial_eps = num_financial_eps
+		self.market = market # ETF|MUTUAL_FUND|COMMODITY|INDEX|CRYPTO|FOREX|TSX|AMEX|NASDAQ|NYSE|EURONEXT
 		self.Y = Y
 		self.no_growth_eps = no_growth_eps
 		self.growth_factor = growth_factor
 
 	def get_realtime_stock_price(self):
+		"""Returns real time stock price"""
 		try:
 			r = requests.get('https://financialmodelingprep.com/api/v3/quote/' + self.symbol)
 		except Exception as e:
@@ -21,13 +23,15 @@ class FairValue():
 			return real_time_stock_price
 
 	def get_50_day_average_stock_price(self):
+		"""Returns 50-day average stock price"""
 		try:
 			r = requests.get('https://financialmodelingprep.com/api/v3/quote/' + self.symbol)
 		except Exception as e:
 			print(e)
 		else:
-			50_day_average_stock_price = float(json.loads(r.text)[0]['priceAvg50'])
-			return 50_day_average_stock_price
+			average_stock_price_50_day = float(json.loads(r.text)[0]['priceAvg50'])
+
+		return average_stock_price_50_day
 
 	def get_eps(self):
 		pass
@@ -35,17 +39,26 @@ class FairValue():
 	def get_average_eps(self):
 		pass
 
+	def get_expected_growth(self):
+		pass
+
 	def company_fair_value(self):
-		fair_value = self.get_average_eps() * (self.no_growth_eps + self.growth_factor * self.expected_growth()) * 4.4 / self.Y
+		"""Returns the fair value of the company"""
+		fair_value = self.get_average_eps() * (self.no_growth_eps + self.growth_factor * self.get_expected_growth()) * 4.4 / self.Y
 
 		return fair_value
 
 class Symbols:
-	def __init__(self):
-		pass
+	def __init__(self, market):
+		self.market = market
 
 	def return_all_symbols(self):
-		pass
+		r = requests.get('https://financialmodelingprep.com/api/v3/search?query=&exchange=' + self.market)
+		symbols = []
+		for item in json.loads(r.text):
+			symbols.append(item['symbol'])
+
+		return symbols
 
 class UndervaluedCompanies():
 	def __init__(self, fair_value, actual_value):
