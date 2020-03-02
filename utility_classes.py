455b0f0cd6d268aae11db922d012aab073ae6e02
avkhimen <avkhimen@ualberta.ca>
2020-03-02 14:33:18 -0700

    Created working 'get_real_time_stock_price' and 'get_50_day_average_stock_price' functions

--- utility_classes.py
+++ utility_classes.py
@@ -1,11 +1,33 @@
-
+import requests
+import json
+from pprint import pprint
 
 class FairValue():
-	def __init__(self, symbol, regression_data_points, num_financial_eps, Y = 1.8, growth_factor = 1):
-		pass
+	def __init__(self, symbol, regression_data_points, num_financial_eps, Y = 1.8, no_growth_eps = 7, growth_factor = 1):
+		self.symbol = symbol
+		self.regression_data_points = regression_data_points
+		self.num_financial_eps = num_financial_eps
+		self.Y = Y
+		self.no_growth_eps = no_growth_eps
+		self.growth_factor = growth_factor
 
 	def get_realtime_stock_price(self):
-		pass
+		try:
+			r = requests.get('https://financialmodelingprep.com/api/v3/quote/' + self.symbol)
+		except Exception as e:
+			print(e)
+		else:
+			real_time_stock_price = float(json.loads(r.text)[0]['price'])
+			return real_time_stock_price
+
+	def get_50_day_average_stock_price(self):
+		try:
+			r = requests.get('https://financialmodelingprep.com/api/v3/quote/' + self.symbol)
+		except Exception as e:
+			print(e)
+		else:
+			50_day_average_stock_price = float(json.loads(r.text)[0]['priceAvg50'])
+			return 50_day_average_stock_price
 
 	def get_eps(self):
 		pass
@@ -13,11 +35,10 @@ class FairValue():
 	def get_average_eps(self):
 		pass
 
-	def calculate_average_eps(self):
-		pass
-
 	def company_fair_value(self):
-		fair_value = 
+		fair_value = self.get_average_eps() * (self.no_growth_eps + self.growth_factor * self.expected_growth()) * 4.4 / self.Y
+
+		return fair_value
 
 class Symbols:
 	def __init__(self):
@@ -27,14 +48,17 @@ class Symbols:
 		pass
 
 class UndervaluedCompanies():
-	def __init__(self):
+	def __init__(self, fair_value, actual_value):
 		pass
-
-	def is_company_undervalued(self):
-		if 
 
 	def return_undervalued_company(self):
 		if self.is_company_undervalued():
+			return True
+		else:
+			return False
+
+	def is_company_undervalued(self):
+		if self.fair_value < self.actual_value:
 			return True
 		else:
 			return False
