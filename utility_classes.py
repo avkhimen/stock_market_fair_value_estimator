c5430d5d877d6fbac743c857655a85c3538d4024
avkhimen <avkhimen@ualberta.ca>
2020-03-02 13:58:16 -0700

    Introduced 'UndervaluedCompanies' class and refactored 'FairValue' class

--- utility_classes.py
+++ utility_classes.py
@@ -1,14 +1,23 @@
 
 
 class FairValue():
-	def __init__(self, symbol, regression_data_points, num_financial_eps):
+	def __init__(self, symbol, regression_data_points, num_financial_eps, Y = 1.8, growth_factor = 1):
 		pass
 
-	def grab_real_time_stock_price(self):
+	def get_realtime_stock_price(self):
 		pass
 
-	def grab_financial_statements(self):
+	def get_eps(self):
 		pass
+
+	def get_average_eps(self):
+		pass
+
+	def calculate_average_eps(self):
+		pass
+
+	def company_fair_value(self):
+		fair_value = 
 
 class Symbols:
 	def __init__(self):
@@ -16,3 +25,17 @@ class Symbols:
 
 	def return_all_symbols(self):
 		pass
+
+class UndervaluedCompanies():
+	def __init__(self):
+		pass
+
+	def is_company_undervalued(self):
+		if 
+
+	def return_undervalued_company(self):
+		if self.is_company_undervalued():
+			return True
+		else:
+			return False
+
